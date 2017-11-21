4c5f95fcda09d0a2867a13f6b8ad75d7d8ec89d5
fidofigo <394730097@qq.com>
2017-11-21 15:20:54 +0800

    更新

--- service/src/main/java/com/senhome/service/account/service/AccountServiceImpl.java
+++ service/src/main/java/com/senhome/service/account/service/AccountServiceImpl.java
@@ -11,12 +11,42 @@ import org.springframework.stereotype.Service;
 @Service("accountServiceApi")
 public class AccountServiceImpl implements AccountServiceApi
 {
-
     @Autowired
     private AccountBusiness accountBusiness;
 
     @Override
     public ViewResult login(String email, String pwd)
+    {
+        ViewResult viewResult = ViewResult.ofSuccess();
+
+        Account account = accountBusiness.findByEmail(email);
+
+        if(account == null)
+        {
+            viewResult.setSuccess(false);
+            viewResult.setMessage("account not exist");
+            return viewResult;
+        }
+        else
+        {
+            if(pwd.equals(account.getPwd()))
+            {
+                AccountDTO accountDTO = new AccountDTO();
+                accountDTO.setAccountId(account.getId());
+
+                return ViewResult.ofSuccess().putDefaultModel(accountDTO);
+            }
+            else
+            {
+                viewResult.setSuccess(false);
+                viewResult.setMessage("password error");
+                return viewResult;
+            }
+        }
+    }
+
+    @Override
+    public ViewResult register(String email, String pwd)
     {
         ViewResult viewResult = ViewResult.ofSuccess();
 
@@ -45,37 +75,6 @@ public class AccountServiceImpl implements AccountServiceApi
             {
                 viewResult.setSuccess(false);
                 viewResult.setMessage("login fail");
-                return viewResult;
-            }
-        }
-    }
-
-    @Override
-    public ViewResult register(String email, String pwd)
-    {
-        ViewResult viewResult = ViewResult.ofSuccess();
-
-        Account account = accountBusiness.findByEmail(email);
-
-        if(account == null)
-        {
-            viewResult.setSuccess(false);
-            viewResult.setMessage("account not exist");
-            return viewResult;
-        }
-        else
-        {
-            if(pwd.equals(account.getPwd()))
-            {
-                AccountDTO accountDTO = new AccountDTO();
-                accountDTO.setAccountId(account.getId());
-
-                return ViewResult.ofSuccess().putDefaultModel(accountDTO);
-            }
-            else
-            {
-                viewResult.setSuccess(false);
-                viewResult.setMessage("password error");
                 return viewResult;
             }
         }
