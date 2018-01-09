2459c1c4aaacc81017a5aacc67c97454c9686893
fidofigo <394730097@qq.com>
2018-01-09 18:18:29 +0800

    更新商家端登录

--- web/src/main/java/com/senhome/web/account/controller/ShopAccountController.java
+++ web/src/main/java/com/senhome/web/account/controller/ShopAccountController.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/appNative/account")
+@RequestMapping("/appNative/shopAccount")
 public class ShopAccountController
 {
     @Resource
     private AccountServiceApi accountServiceApi;
 
-    @RequestMapping(value = "/shopLogin", method = RequestMethod.POST)
-    public Object shopLogin(AccountParam accountParam)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Object login(AccountParam accountParam)
     {
         ViewResult result = accountServiceApi.shopLogin(accountParam.getMobileNumber(), accountParam.getPwd());
 
